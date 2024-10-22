8978f990484174de6793249d5bae67ad1449ca3d
Jan Atle Ramsli <jaramsli@gmail.com>
2024-10-22 09:51:53 +0200

    Added a SPAWN example

--- spawn.fs
+++ spawn.fs
@@ -1,6 +1,19 @@
-: tst 1 . 2 . 3 . ;
-DEFER tst
-SPAWN
-4 .
-5 .
-6 .
+: task0 30 0 DO 0 . LOOP CR " Task0 done!" s. CR ;
+: task1 30 0 DO 1 . LOOP CR " Task1 done!" s. CR ;
+: task2 30 0 DO 2 . LOOP CR " Task2 done!" s. CR ;
+: task3 30 0 DO 3 . LOOP CR " Task3 done!" s. CR ;
+CR " These are the tasks to run:" s. 
+DICT
+" First, we run them in sequence:" s. CR
+' task0 EXEC
+' task1 EXEC
+' task2 EXEC
+' task3 EXEC
+CR " Then, we run them concurrently:" s. CR
+' task0 SPAWN
+' task1 SPAWN
+' task2 SPAWN
+' task3 SPAWN
+
+10000 0 DO   LOOP
+CR " Now we wait for them to finish" s.
